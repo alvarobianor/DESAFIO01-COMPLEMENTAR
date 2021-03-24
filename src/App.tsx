d9f48c10cc6285e648a29cb0aa273041c894b4fd
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-03-23 22:59:12 -0300

    Cleaning comments

--- src/App.tsx
+++ src/App.tsx
@@ -69,27 +69,6 @@ export function App() {
       />
 
       <Content movies={movies} selectedGenre={selectedGenre} />
-      {/* <div className="container">
-        <header>
-          <span className="category">
-            Categoria:<span> {selectedGenre.title}</span>
-          </span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map((movie) => (
-              <MovieCard
-                key={movie.imdbID}
-                title={movie.Title}
-                poster={movie.Poster}
-                runtime={movie.Runtime}
-                rating={movie.Ratings[0].Value}
-              />
-            ))}
-          </div>
-        </main>
-      </div> */}
     </div>
   );
 }
